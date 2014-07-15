27772bb80f2e9e66490a4287b0acc2ca1000e0f6
alexandre.tranchant <Alexandre.Tranchant@gmail.com>
2014-07-15 14:23:43 +0200

    [features/french] French translation 2 new files config/module.config.php was updated to load the language

--- config/module.config.php
+++ config/module.config.php
@@ -115,4 +115,14 @@ return array(
             ),
         ),
     ),
+    'translator' => array(
+    		'translation_file_patterns' => array(
+    				array(
+    						'type'     => 'gettext',
+    						'base_dir' => __DIR__ . '/../language',
+    						'pattern'  => '%s.mo',
+    						'text_domain' => 'csnuser',
+    				),
+    		),
+    ),
 );
